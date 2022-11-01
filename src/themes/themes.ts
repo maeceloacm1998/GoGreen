3a3a990bbac0246afbde3253a3a04aa5b224d816
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-11-01 00:49:13 -0300

    chore: add theme colors

---
+++ src/themes/themes.ts
@@ -0,0 +1,10 @@
+export default {
+  color: {
+    primary: '#32B768',
+    primary_light_medium: "#32b76753",
+    primary_light_low: "#32b76736",
+    category_tag_color: "#2B7A4B",
+    text_color: "#5C6660",
+    background_card_color: "#F8F8F8",
+  }
+}

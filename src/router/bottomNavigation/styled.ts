93b8e5a815cbd6591ac7e7dfdfbb107437264c7d
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-11-15 00:58:55 -0300

    refactor: add new style bottom navigation

--- src/router/bottomNavigation/styled.ts
+++ src/router/bottomNavigation/styled.ts
@@ -1,14 +1,31 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
-import themes from "../../themes/themes";
+import themes from '../../themes/themes';
 
 export type TextOptionType = {
-  focused: boolean
-}
+  focused: boolean;
+  color: string;
+};
+
+export type ContainerIcon = {
+  color?: string;
+};
 
 export const TextOption = styled.Text<TextOptionType>`
   font-size: 13px;
   font-family: ${themes.fonts.jost_medium};
   font-weight: 700;
-  color: ${props => props.focused ? themes.color.primary_dark : themes.color.primary};
+  color: ${props =>
+    props.focused ? themes.color.primary_dark : themes.color.primary};
+  color: ${props => props.color};
+`;
+
+export const ContainerIcon = styled.View<ContainerIcon>`
+  margin-bottom: -15px;
+  width: 90%;
+  height: 60px;
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => (props.color ? props.color : '#FFFFFF')};
 `;

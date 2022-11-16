9c4a8a9b16ab7c7d6383f61828797cbfa125bf69
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-11-15 23:49:44 -0300

    feat: add filter list provider in all user routes

--- src/router/main.routes.tsx
+++ src/router/main.routes.tsx
@@ -3,27 +3,30 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import ButtonNavigation from './bottomNavigation/buttomNavigation.routes';
 import FilterList from '../screens/user/FilterList';
+import {FilterListProviderHook} from '../context';
 
 const Stack = createStackNavigator();
 
 function MainRoute() {
   return (
-    <Stack.Navigator initialRouteName="InitialRouteUser">
-      <Stack.Screen
-        name="InitialRouteUser"
-        component={ButtonNavigation}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="FilterList"
-        component={FilterList}
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack.Navigator>
+    <FilterListProviderHook>
+      <Stack.Navigator initialRouteName="InitialRouteUser">
+        <Stack.Screen
+          name="InitialRouteUser"
+          component={ButtonNavigation}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="FilterList"
+          component={FilterList}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </FilterListProviderHook>
   );
 }
 

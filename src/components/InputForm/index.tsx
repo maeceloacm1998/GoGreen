b8c107c384bf84079d75d452e21ef2249cca314e
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-11-28 20:41:34 -0300

    feat: add input form component

---
+++ src/components/InputForm/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {TextInputProps} from 'react-native';
+
+import {
+  Controller,
+  Control,
+  FieldError,
+  FieldErrorsImpl,
+  Merge,
+} from 'react-hook-form';
+
+import {Container, ErrorContainer, ErrorText, TextInputCustom} from './styled';
+
+interface Props extends TextInputProps {
+  nameId: string;
+  placeholder: string;
+  control: Control;
+  formState: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined;
+  requiredRule: boolean;
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
+}
+
+export function InputForm({
+  nameId,
+  placeholder,
+  control,
+  formState,
+  requiredRule,
+  marginTop,
+  marginBottom,
+  marginLeft,
+  marginRight,
+  ...rest
+}: Props) {
+  return (
+    <Container
+      marginTop={marginTop}
+      marginBottom={marginBottom}
+      marginLeft={marginLeft}
+      marginRight={marginRight}>
+      <Controller
+        control={control}
+        rules={{
+          required: requiredRule,
+        }}
+        render={({field: {onChange, value}}) => (
+          <TextInputCustom
+            placeholder={requiredRule ? `${placeholder} *` : placeholder}
+            onChangeText={onChange}
+            value={value}
+            {...rest}
+          />
+        )}
+        name={nameId}
+      />
+      {formState && (
+        <ErrorContainer>
+          <ErrorText>Esse campo é obrigatório</ErrorText>
+        </ErrorContainer>
+      )}
+    </Container>
+  );
+}
---
+++ src/components/InputForm/styled.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+import themes from '../../themes/themes';
+
+export type TextInputCustomType = {
+  marginTop: number;
+  marginBottom: number;
+  marginLeft: number;
+  marginRight: number;
+};
+
+export const Container = styled.View<TextInputCustomType>`
+  width: 100%;
+  margin-top: ${props => (props.marginTop ? props.marginTop : 0)}px;
+  margin-bottom: ${props => (props.marginBottom ? props.marginBottom : 0)}px;
+  margin-left: ${props => (props.marginLeft ? props.marginLeft : 0)}px;
+  margin-right: ${props => (props.marginRight ? props.marginRight : 0)}px;
+`;
+
+export const TextInputCustom = styled.TextInput`
+  height: 48px;
+  width: 100%;
+  border-radius: 5px;
+  padding: 0 16px;
+  color: ${themes.color.text_color};
+  background-color: ${themes.color.primary_light_low};
+`;
+
+export const ErrorContainer = styled.View`
+  width: 100%;
+`;
+
+export const ErrorText = styled.Text`
+  font-size: 12px;
+  font-family: ${themes.fonts.jost_regular};
+  font-weight: 500;
+  color: ${themes.color.error};
+`;

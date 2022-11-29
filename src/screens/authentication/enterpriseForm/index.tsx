8bcadcc4c11e3499ac1279a178eef026c79da0d7
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-11-29 19:46:47 -0300

    feat: add input form component with react hook for

---
+++ src/screens/authentication/enterpriseForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {Container} from './styled';
+
+const EnterpriseForm = () => {
+  return (
+    <Container>
+      <Text>DAle dale dale dale</Text>
+    </Container>
+  );
+};
+
+export default EnterpriseForm;

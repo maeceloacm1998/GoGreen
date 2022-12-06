b25b78dd8ee2c29ebeddec479195b81dfc089913
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-12-05 21:20:14 -0300

    feat: add company data in authentication context

--- src/context/Authentication/index.tsx
+++ src/context/Authentication/index.tsx
@@ -1,15 +1,18 @@
 import React, {createContext, useContext, useState, useMemo} from 'react';
 
 interface AuthenticationContextData {
-  authenticationUser(user: User): void;
+  authentication(body: User | Company, userType: string): void;
   logout(): void;
   user: User;
-  logged: boolean;
+  company: Company;
+  loggedUser: boolean;
+  loggedCompany: boolean;
 }
 
 export interface User {
   id: number;
   name: string;
+  userType: string;
   city: string;
   state: string;
   cep: string;
@@ -19,43 +22,64 @@ export interface User {
   password: string;
 }
 
+export interface Company {
+  id: number;
+  name: string;
+  userType: string;
+  imageUrl: string;
+  category: string;
+  city: string;
+  state: string;
+  cep: string;
+  address: string;
+  date: string;
+  email: string;
+  password: string;
+  description: string;
+}
+
 const AuthenticationContext = createContext<AuthenticationContextData>(
   {} as AuthenticationContextData,
 );
 
+export const userTypeProps = {
+  user: 'User',
+  company: 'Company',
+};
+
 export const AuthenticationProvider = ({children}: any) => {
   const [user, setUser] = useState<User>({} as User);
-  const [logged, setLogged] = useState<boolean>(false);
+  const [company, setCompany] = useState<Company>({} as Company);
+  const [loggedUser, setLoggedUser] = useState<boolean>(false);
+  const [loggedCompany, setLoggedCompany] = useState<boolean>(false);
 
-  async function authenticationUser(user: User) {
-    const fakeUser: User = {
-      id: 1,
-      cep: '31550500',
-      city: 'Belo Horizonte',
-      name: 'Marcelo',
-      password: '12341',
-      email: 'maeceloacm1998@gmail.com',
-      state: 'Minas Gerais',
-      address: 'Rua hildebrando de oliveira, 234',
-      date: 'Data',
-    };
-
-    setUser(fakeUser);
-    setLogged(true);
+  async function authentication(body: User | Company) {
+    if (body.userType === userTypeProps.user) {
+      setUser(body as User);
+      setLoggedUser(true);
+    } else {
+      setCompany(body as Company);
+      setLoggedCompany(true);
+    }
   }
 
   function logout() {
-    setLogged(false);
-    setUser({} as User);
+    if (loggedUser) {
+      setLoggedUser(false);
+    } else {
+      setLoggedCompany(false);
+    }
   }
 
   return (
     <AuthenticationContext.Provider
       value={{
-        authenticationUser,
-        logged,
+        authentication,
+        loggedUser,
+        loggedCompany,
         logout,
         user,
+        company,
       }}>
       {children}
     </AuthenticationContext.Provider>

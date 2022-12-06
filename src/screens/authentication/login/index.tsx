1f65717fa6ce7a846803d92b87423cc4f846e4fa
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-12-05 21:24:53 -0300

    feat: add mocky login user

--- src/screens/authentication/login/index.tsx
+++ src/screens/authentication/login/index.tsx
@@ -7,6 +7,12 @@ import {ScreenProps} from '../../../router/models/ScreenPropsModel';
 import ImagePNG from '../../../assets/images/plant_login.png';
 
 import {
+  useAuthentication,
+  User,
+  userTypeProps,
+} from '../../../context/Authentication';
+
+import {
   ButtonDefault,
   ButtonWithBorder,
   Container,
@@ -14,15 +20,27 @@ import {
   TextButton,
 } from './styled';
 import themes from '../../../themes/themes';
-import {useAuthentication} from '../../../context/Authentication';
 
 const Login = ({navigation}: ScreenProps) => {
   const registerNavigation = () => navigation.navigate('SelectionRegisterType');
 
-  const {authenticationUser} = useAuthentication();
+  const {authentication} = useAuthentication();
 
   async function login() {
-    authenticationUser('Dale', '123123');
+    const fakeUser: User = {
+      id: 1,
+      cep: '31550500',
+      city: 'Belo Horizonte',
+      name: 'Marcelo',
+      userType: userTypeProps.user,
+      password: '12341',
+      email: 'maeceloacm1998@gmail.com',
+      state: 'Minas Gerais',
+      address: 'Rua hildebrando de oliveira, 234',
+      date: 'Data',
+    };
+
+    authentication(fakeUser, userTypeProps.user);
   }
 
   return (
